e8798f61d6480c7809301eec7301bbebb2c10d02
Nitish Kumar Singh <nitishk72@gmail.com>
2017-12-18 12:22:52 +0530

    today final  comimit
    will work later on this project

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const compression = require('compression');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -6,12 +7,17 @@ const sass = require('node-sass-middleware');
 
 const port = 3000;
 
-app.use(sass({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public')
-  }));
+app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(compression());
+app.use(sass({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public')
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const homeController = require('./controllers/home');
 
